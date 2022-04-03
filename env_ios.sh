73880262ee84cf41b5e602405b377c4a6bb10033
2tefan <2tefan@tuta.io>
2022-04-03 17:43:30 +0200

    Update version code

--- env_ios.sh
+++ env_ios.sh
@@ -4,6 +4,6 @@ set -a # Export variables
 : "${FONT_VERSION:=15.4}"
 : "${PACKAGE_VERSION:=3}"
 
-: "${VERSION_CODE:=2}"
+: "${VERSION_CODE:=3}"
 : "${FONT:=ios}"
 set +a # Stop
--- env_noto.sh
+++ env_noto.sh
@@ -4,6 +4,6 @@ set -a # Export variables
 : "${FONT_VERSION:=2.034}"
 : "${PACKAGE_VERSION:=3}"
 
-: "${VERSION_CODE:=2}"
+: "${VERSION_CODE:=3}"
 : "${FONT:=noto}"
 set +a # Stop
