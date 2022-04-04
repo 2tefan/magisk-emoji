3b8a9e0a8d943041fb246c224da78210168a28da
2tefan <2tefan@tuta.io>
2022-04-04 21:41:14 +0200

    Add blobmoji

---
+++ buildBlobmoji.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. $(dirname "$0")/common.sh
+. $(dirname "$0")/env_blobmoji.sh
+parse_args "$@"
---
+++ env_blobmoji.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -a # Export variables
+: "${FONT_VERSION:=14.0.1}"
+: "${PACKAGE_VERSION:=1}"
+
+: "${VERSION_CODE:=1}"
+: "${FONT:=blobmoji}"
+set +a # Stop
